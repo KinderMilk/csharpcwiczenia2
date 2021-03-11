f89baff4d93e2e36f433ff37e074b84a64c7241f
Mateusz <papierekm@gmail.com>
2021-03-11 09:42:55 +0100

    Update Program.cs

--- csharplaby/Program.cs
+++ csharplaby/Program.cs
@@ -6,14 +6,14 @@ namespace csharplaby
     {
         static void Main(string[] args)
         {
-            string imie;
+            
             Console.WriteLine("Witaj!");
             Console.WriteLine("Podaj swoję Imię: ");
 
-            imie = Console.ReadLine();
+            string imie = Console.ReadLine();
             Console.WriteLine("Podaj swoje nazwisko");
             string nazwisko = Console.ReadLine();
-            Console.WriteLine($"Witaj {0}, {1}"imie,nazwisko);
+            Console.WriteLine("Witaj {0}, {1}" , imie,nazwisko);
             
 
     }
